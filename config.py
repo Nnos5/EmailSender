31a0b0c09b5846b35409b3724a4e47fb621f75dd
NNOS <nnos@localhost.localdomain>
2025-08-21 18:18:27 +0200

    premier essai ok

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+EMAIL_ADDRESS = "ndeunas@gmail.com"
+EMAIL_PASSWORD = "uktl asmm gnuf pffy"  # 🔒 Utilise un mot de passe spécifique aux apps pour Gmail
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
